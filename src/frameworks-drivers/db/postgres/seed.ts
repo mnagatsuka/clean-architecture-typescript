17197d3bba600a00b0b4cc301eefb8d5200088e9
長塚元規 <motoki@mbp.local>
2025-06-26 10:31:07 +0900

    feat: create next app

--- src/frameworks-drivers/db/postgres/seed.ts
+++ src/frameworks-drivers/db/postgres/seed.ts
@@ -1,9 +1,13 @@
 // src/frameworks-drivers/database/seed.ts
-import { db } from './client'
+import { getDb } from './client'
 import { user as userTable } from './schema'
 import { UserStatus } from '@entities/user/user-status'
 
+const dbUrl = process.env.DB_URL ?? ''
+
 async function seed() {
+  const db = getDb(dbUrl)
+
   await db.insert(userTable).values([
     {
       id: '1',
