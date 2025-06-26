959a726902d8e2d6872dad30d5d9ba3c9e882dd8
長塚元規 <motoki@mbp.local>
2025-06-26 10:34:17 +0900

    create next app

--- src/frameworks-drivers/web/react/vite.config.ts
+++ src/frameworks-drivers/web/react/vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-// import path from 'node:path'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  // resolve: {
-  //   alias: {
-  //     '@': path.resolve(__dirname, './src'),
-  //   },
-  //   extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
-  // },
   server: {
     port: 5173,
     open: true,
